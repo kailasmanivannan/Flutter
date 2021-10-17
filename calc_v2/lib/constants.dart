d5d2703fe12ec3a65ab6d9d432d54044a5aa4451
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-10-17 18:12:16 +0530

    Add constants dart file

---
+++ calc_v2/lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const buttonTextStyle = TextStyle(fontSize: 25, fontWeight: FontWeight.bold);
+const borderSide = BorderSide(color: Colors.black, width: 2);
